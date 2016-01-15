74d329f253a1f748c0f028cb2ab6899a0abd5a20
Sandmeert <satanderm@gmail.com>
2016-01-15 11:02:06 +0100

    Added slider, updated CSS and small tweaks

--- js/app.js
+++ js/app.js
@@ -52,5 +52,16 @@ $(function() {
             }
         }
     });
+    
+    /**
+     * Initialise slideshow
+     */
+    
+    $('.pgwSlider').pgwSlider({
+        displayList: false,
+        autoSlide: false, 
+        touchControls: true,
+        displayControls: true
+    });
   
 });
