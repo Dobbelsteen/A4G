6504b2a9233239836ebb8392aa2094d6a1f925d5
Sandmeert <satanderm@gmail.com>
2016-01-15 11:42:11 +0100

    Added slideshow for pictures, responsive navigation

--- js/app.js
+++ js/app.js
@@ -29,28 +29,31 @@ $(function() {
     } // this for loop fills the aArray with attribute href values
 
     $(window).scroll(function(){
-        var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
-        var windowHeight = $(window).height(); // get the height of the window
-        var docHeight = $(document).height();
+        if($(window).width() > 767) {
+            var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+            var windowHeight = $(window).height(); // get the height of the window
+            var docHeight = $(document).height();
 
-        for (var i=0; i < aArray.length; i++) {
-            var theID = aArray[i];
-            var divPos = $(theID).offset().top - 75; // get the offset of the div from the top of page
-            var divHeight = $(theID).height(); // get the height of the div in question
-            if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
-                $("a[href='" + theID + "']").addClass("active");
-            } else {
-                $("a[href='" + theID + "']").removeClass("active");
+            for (var i=0; i < aArray.length; i++) {
+                var theID = aArray[i];
+                var divPos = $(theID).offset().top - 75; // get the offset of the div from the top of page
+                var divHeight = $(theID).height(); // get the height of the div in question
+                if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+                    $("a[href='" + theID + "']").addClass("active");
+                } else {
+                    $("a[href='" + theID + "']").removeClass("active");
+                }
+            }
+
+            if(windowPos + windowHeight == docHeight) {
+                if (!$("nav li:last-child a").hasClass("active")) {
+                    var navActiveCurrent = $(".active").attr("href");
+                    $("a[href='" + navActiveCurrent + "']").removeClass("active");
+                    $("nav li:last-child a").addClass("active");
+                }
             }
         }
-
-        if(windowPos + windowHeight == docHeight) {
-            if (!$("nav li:last-child a").hasClass("active")) {
-                var navActiveCurrent = $(".active").attr("href");
-                $("a[href='" + navActiveCurrent + "']").removeClass("active");
-                $("nav li:last-child a").addClass("active");
-            }
-        }
+        
     });
     
     /**
@@ -62,6 +65,16 @@ $(function() {
         autoSlide: false, 
         touchControls: true,
         displayControls: true
+    });
+    
+    /**
+     * Toggle Navigation for mobile
+     */
+    
+    $('#nav-toggle').on('click', function(ev) {
+        ev.preventDefault();
+        $('.nav-mobile-links').slideToggle();
+        return false;
     });
   
 });
